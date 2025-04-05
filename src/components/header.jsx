0a55d99ed995d1295ed11e1ef2a927d9cf6fa8ef
skytechrick <ricksarkar2005@gmail.com>
2025-04-05 15:55:36 +0530

    feat: Header responsive done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,5 @@
 import gsbLogo from "../images/GSB-Full-Logo.webp";
+import menu from "../images/menu.png";
 
 export default () => {
     const headerStyle = {
@@ -12,7 +13,7 @@ export default () => {
 
     return (
         <header style={headerStyle} className="w-full flex justify-center items-center h-14 bg-white font-sans">
-            <div className="w-4/5 flex justify-between items-center">
+            <div className="w-[98%] sm:w-[90%] lg:w-4/5 flex justify-between items-center">
                 <div className="flex justify-between items-center">
                     <div className="w-60 flex justify-center items-center flex-column">
 
@@ -20,32 +21,38 @@ export default () => {
                             <img className="h-12 w-12" src={gsbLogo} alt="GSB logo" />
                         </div>
                         <div className="h-12 relative -top-[2px]">
-                            <h1 className="font-bold text-3xl" style={gsbH1}>GET SKY BUY</h1>
+                            <h1 className="font-bold text-2xl sm:text-3xl" style={gsbH1}>GET SKY BUY</h1>
                             <p className="text-blue-700 relative -top-1 font-left text-sm font-bold italic">Seamless selling<img className="h-4 w-4 inline-block" src={gsbLogo} alt="GSB logo" /></p>
                         </div>
 
                     </div>
 
-                    <div>
-                        <nav className="flex flex-row list-none">
+
+                    <div className="text-sm lg:text-md absolute left-0 top-14 border shadow-sm bg-white w-full flex justify-center items-center lg:static lg:border-0 lg:shadow-none lg:bg-transparent lg:w-auto lg:block lg:justify-start lg:items-start">
+                    {/* <div className="lg:absolute lg:left-0 lg:top-14 lg:border shadow-sm bg-white lg:w-full lg:flex lg:justify-center lg:items-center"> */}
+
+                        <nav className="flex flex-row">
+                            <div className="hidden">
+                                <img src={menu} alt="Meanu" />
+                            </div>
                             <ul className="flex flex-row list-none">
 
-                                <li className="group w-24 mx-1 h-min relative top-0 text-center rounded-lg p-2 bg-white-100 hover:bg-orange-300 transition-all duration-200 cursor-pointer">
+                                <li className="group w-16 mx-0 lg:mx-1 h-min relative top-0 text-center rounded-lg p-1 my-2 lg:p-2 bg-white-100 hover:bg-orange-300 transition-all duration-200 cursor-pointer">
                                     <span>Services</span>
-                                    <ul className="w-[180px] hidden group-hover:block absolute top-10 border left-0 bg-white shadow-xl rounded-lg p-2 transition-all duration-200 ">
-                                        <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">Apply for seller</li>
+                                    <ul role="menu" aria-haspopup="true" className="w-[180px] hidden group-hover:block absolute top-10 border left-0 bg-white shadow-xl rounded-lg p-2 transition-all duration-200 ">
+                                        <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">Register for seller</li>
                                         <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">Business connect</li>
                                     </ul>
                                 </li>
 
-                                <li className="group w-24 mx-1 h-min relative top-0 text-center rounded-lg p-2 bg-white-100 hover:bg-orange-300 transition-all duration-200 cursor-pointer">
+                                <li className="group w-20 mx-0 lg:mx-1 h-min relative top-0 text-center rounded-lg p-1 my-2 lg:p-2 bg-white-100 hover:bg-orange-300 transition-all duration-200 cursor-pointer">
                                     <span>About us</span>
                                     <ul className="w-[180px] hidden group-hover:block absolute top-10 border left-0 bg-white shadow-xl rounded-lg p-2 transition-all duration-200 ">
                                         <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">About seller's platform</li>
                                         <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">About company</li>
                                     </ul>
                                 </li>
-                                <li className="group w-24 mx-1 h-min relative top-0 text-center rounded-lg p-2 bg-white-100 hover:bg-orange-300 transition-all duration-200 cursor-pointer">
+                                <li className="group w-20 mx-0 lg:mx-1 h-min relative top-0 text-center rounded-lg p-1 my-2 lg:p-2 bg-white-100 hover:bg-orange-300 transition-all duration-200 cursor-pointer">
                                     <span>Features</span>
                                     <ul className="w-[180px] hidden group-hover:block absolute top-10 border left-0 bg-white shadow-xl rounded-lg p-2 transition-all duration-200 ">
                                         <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">Large market</li>
@@ -55,9 +62,9 @@ export default () => {
                                         <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">24/7 Support</li>
                                     </ul>
                                 </li>
-                                <li className="group w-24 mx-1 h-min relative top-0 text-center rounded-lg p-2 bg-white-100 hover:bg-orange-300 transition-all duration-200 cursor-pointer">
+                                <li className="group w-20 mx-0 lg:mx-1 h-min relative top-0 text-center rounded-lg p-1 my-2 lg:p-2 bg-white-100 hover:bg-orange-300 transition-all duration-200 cursor-pointer">
                                     <span>Support</span>
-                                    <ul className="w-[180px] hidden group-hover:block absolute top-10 border left-0 bg-white shadow-xl rounded-lg p-2 transition-all duration-200 ">
+                                    <ul className="w-[180px] hidden group-hover:block absolute top-10 border right-0 bg-white shadow-xl rounded-lg p-2 transition-all duration-200 ">
                                         <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">WhatsApp us</li>
                                         <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">Help center</li>
                                         <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">Mail us</li>
@@ -68,12 +75,12 @@ export default () => {
                     </div>
                 </div>
                 <div className="flex flex-row">
-                    <div className="group w-24 mx-1 h-min relative top-0 text-center rounded-lg shadow-sm p-2 bg-white-100 bg-orange-300 hover:bg-white hover:border hover:underline active:bg-orange-200 transition-all duration-200 cursor-pointer">
+                    <div className="hidden sm:block group w-24 mx-0 lg:mx-1 h-min relative top-0 text-center rounded-lg shadow-sm p-1 my-2 lg:p-2 bg-white-100 bg-orange-300 hover:bg-white hover:border hover:underline active:bg-orange-200 transition-all duration-200 cursor-pointer">
                         <span>Register</span>    
                     </div>
-                    <div className="group w-24 mx-1 h-min relative top-0 text-center rounded-lg p-2 bg-white-100 hover:bg-orange-300 transition-all duration-200 cursor-pointer">
+                    <div className="group w-20 mx-0 lg:mx-1 h-min relative top-0 text-center rounded-lg p-1 my-2 lg:p-2 bg-white hover:bg-orange-300 transition-all duration-200 cursor-pointer">
                         <span>Login</span>
-                        <ul className="w-[180px] hidden group-hover:block absolute top-10 border left-0 bg-white shadow-xl rounded-lg p-2 transition-all duration-200 ">
+                        <ul className="w-[180px] hidden group-hover:block absolute right-0 top-10 border bg-white shadow-xl rounded-lg p-2 transition-all duration-200 ">
                             <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">Seller's Login</li>
                             <li className="p-1 active:bg-gray-300 active:underline text-sm hover:bg-gray-200 rounded-md mb-1">Onboarder's login</li>
                         </ul>

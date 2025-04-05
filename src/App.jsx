6eb9f839aa4b3fb6c49622a8d4169c4dc888a748
skytechrick <ricksarkar2005@gmail.com>
2025-04-06 00:25:17 +0530

    feat: wip Seller login - TODO

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import LoginPage from './components/login.jsx';
 import HomeHeader from './components/homeHeader.jsx';
 import HomePage from './components/homePage.jsx';
 import Onboarder from './components/onboarding.jsx';
+import HomePageSection7 from "./components/homePageSection7.jsx";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,9 +11,13 @@ function App() {
             {/* <div className='flex flex-col'></div> */}
             <Route path="/" element={<HomePage />} />
                 
-            {/* <Route path="/onboarding" element={<HomeHeader />} > */}
+            <Route path="/auth">
+                <Route path="login" element={<><HomeHeader/><LoginPage /> <HomePageSection7/></>} />
+            </Route>
 
-                {/* <Route path="login" element={<LoginPage />} /> */}
+            {/* <Route path="/onboarding"> */}
+
+                {/* <Route path="login" element={<><HomeHeader/><LoginPage /></>} /> */}
 
             {/* </Route> */}
 
